bfaba4c7b40baaee3d796a6145e4d7927cf39523
Muhammad Umer <157574032+UMERZAFAR777@users.noreply.github.com>
2024-09-29 19:56:51 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -216,5 +216,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'umerzafar433@gmail.com'
-EMAIL_HOST_PASSWORD = 'wpbbxbwxtbxujszh'  # App Password
+EMAIL_HOST_PASSWORD = ''  # App Password
 
