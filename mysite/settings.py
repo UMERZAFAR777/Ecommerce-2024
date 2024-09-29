cfa7fba38732f46c51143530f413d2fadf704a8f
Muhammad Umer <157574032+UMERZAFAR777@users.noreply.github.com>
2024-09-29 20:01:11 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -209,7 +209,7 @@ LOGOUT_REDIRECT_URL = 'home'
 # EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_PORT = 587
 # EMAIL_HOST_USER = 'umerzafar433@gmail.com'
-# EMAIL_HOST_PASSWORD = 'wpbbxbwxtbxujszh'
+# EMAIL_HOST_PASSWORD = ''
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
